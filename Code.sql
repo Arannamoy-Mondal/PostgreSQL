ddfa3a89042fdff79edc1624bb57002c091b5090
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2025-05-14 22:37:58 +0600

    upd

--- Code.sql
+++ Code.sql
@@ -1,14 +1,9 @@
--- Copyright 2025 Arannamoy Mondal
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
 CREATE TABLE PERSON (
   ID SERIAL NOT NULL,
   USER_NAME VARCHAR(255),
   AGE INT,
+  EMAIL VARCHAR(200) UNIQUE ,
+ CONSTRAINT  PERSON_EMAIL_CHECK CHECK ( EMAIL LIKE '%@%.COM'),
   CONSTRAINT PERSON_AGE CHECK ( AGE>18 ),
   CONSTRAINT PERSON_PK_RULE PRIMARY KEY (ID)
 );
